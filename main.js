498408a03c07013416778b60a79fbc5dcdc25d5f
atas <atasnovruzlu>
2018-11-19 11:52:50 +0400

    This is a new commit for what I originally planned to be amended

---
+++ main.js
@@ -0,0 +1,20 @@
+function insert(num){
+     document.getElementById("tex").value = document.getElementById("tex").value+num;
+}
+
+function equal(){
+    var exp=document.getElementById("tex").value;
+    if(exp){
+ document.getElementById("tex").value=eval(exp);
+}
+}
+
+
+function clean(){
+    document.getElementById("tex").value=" ";
+}
+
+function back(){
+   var exp=document.getElementById("tex").value;
+   document.getElementById("tex").value=exp.substring(0,exp.length-1);
+}
